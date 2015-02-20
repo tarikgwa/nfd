f4cde18d456c27e0885b86367b3e3615a1335d40
tarikgwa <aboualitarik@gmail.com>
2015-02-20 15:35:07 +0000

    nfd

---
+++ install/install-all.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+#
+# Newfies-Dialer License
+# http://www.newfies-dialer.org
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2014 Star2Billing S.L.
+#
+# The primary maintainer of this project is
+# Arezqui Belaid <info@star2billing.com>
+#
+
+#
+# To download and run the script on your server :
+# cd /usr/src/ ; rm install-all.sh ; wget --no-check-certificate https://raw.github.com/Star2Billing/newfies-dialer/master/install/install-all.sh ; chmod +x install-all.sh ; ./install-all.sh
+#
+# BRANCH = develop
+# cd /usr/src/ ; rm install-all.sh ; wget --no-check-certificate https://raw.github.com/Star2Billing/newfies-dialer/develop/install/install-all.sh ; chmod +x install-all.sh ; ./install-all.sh
+#
+
+#Set branch to install develop / master
+BRANCH='master'
+
+SCRIPT_NOTICE="This script is only intended to run on Debian 7.X"
+
+# Identify Linux Distribution type
+func_identify_os() {
+    if [ -f /etc/debian_version ] ; then
+        DIST='DEBIAN'
+        apt-get -y install lsb-release
+        if [ "$(lsb_release -cs)" != "wheezy" ]; then
+            echo $SCRIPT_NOTICE
+            exit 255
+        fi
+    elif [ -f /etc/redhat-release ] ; then
+        DIST='CENTOS'
+        if [ "$(awk '{print $3}' /etc/redhat-release)" != "6.2" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.3" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.4" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.5" ]; then
+            echo $SCRIPT_NOTICE
+            exit 255
+        fi
+    else
+        echo $SCRIPT_NOTICE
+        exit 1
+    fi
+}
+
+#install the epel repository.
+func_install_epel_repo() {
+	if [ ! -f /etc/yum.repos.d/epel.repo ];
+		then
+			echo '
+[epel]
+name=Extra Packages for Enterprise Linux 6 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/6/$basearch
+mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch
+failovermethod=priority
+enabled=0
+gpgcheck=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
+		' > /etc/yum.repos.d/epel.repo
+	fi
+}
+
+#install the rpmforge repository.
+func_install_rpmforge_repo() {
+
+	if [ ! -f /etc/yum.repos.d/rpmforge.repo ];
+		then
+			echo '
+[rpmforge]
+name = RHEL $releasever - RPMforge.net - dag
+baseurl = http://apt.sw.be/redhat/el6/en/$basearch/rpmforge
+mirrorlist = http://apt.sw.be/redhat/el6/en/mirrors-rpmforge
+#mirrorlist = file:///etc/yum.repos.d/mirrors-rpmforge
+enabled = 0
+protect = 0
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag
+gpgcheck = 0
+		' > /etc/yum.repos.d/rpmforge.repo
+	fi
+}
+
+#Identify the OS
+func_identify_os
+
+echo ""
+echo "> > > This is only to be installed on a fresh new installation of Debian 7.X or CentOS 6.X! < < <"
+echo ""
+echo "It will install Newfies-Dialer and Freeswitch on your server"
+echo ""
+echo ""
+echo ""
+
+case $DIST in
+    'DEBIAN')
+        apt-get -y update
+        apt-get -y install vim git-core
+    ;;
+    'CENTOS')
+		func_install_epel_repo
+		func_install_rpmforge_repo
+        yum -y update
+        yum -y install mlocate vim git wget
+        yum -y install policycoreutils-python
+    ;;
+esac
+
+#Install Freeswitch
+cd /usr/src/
+wget --no-check-certificate  https://raw.github.com/tarikgwa/nfd/$BRANCH/install/install-freeswitch.sh -O install-freeswitch.sh
+bash install-freeswitch.sh
+/etc/init.d/freeswitch start
+
+#Install Newfies
+cd /usr/src/
+wget --no-check-certificate https://raw.github.com/tarikgwa/nfd/$BRANCH/install/install-newfies.sh -O install-newfies.sh
+bash install-newfies.sh
---
+++ newfies/dialer_contact/management/commands/create_contact.py
@@ -0,0 +1,89 @@
+#
+# Newfies-Dialer License
+# http://www.newfies-dialer.org
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2014 Star2Billing S.L.
+#
+# The primary maintainer of this project is
+# Arezqui Belaid <info@star2billing.com>
+#
+
+from django.core.management.base import BaseCommand
+from dialer_contact.models import Phonebook, Contact
+from optparse import make_option
+from django.db import IntegrityError
+from random import choice
+
+
+class Command(BaseCommand):
+    args = "phonebook_id, amount"
+    help = "Create a new contacts for a given phonebook\n"\
+           "-------------------------------------------\n"\
+           "python manage.py create_contact --phonebook_id=1 --amount=100 --prefix=@myip"
+
+    option_list = BaseCommand.option_list + (
+        make_option('--amount',
+                    default=None,
+                    dest='amount',
+                    help='Amount to create, by default 1 contact will be created'),
+        make_option('--phonebook_id',
+                    default=None,
+                    dest='phonebook_id',
+                    help='Phonebook ID for the new contact'),
+        make_option('--prefix',
+                    default=None,
+                    dest='prefix',
+                    help='Prefix to be added after the phonenumber, ie. @myip'),
+    )
+
+    def handle(self, *args, **options):
+        """
+        Note that contacts created this way are only for devel purposes
+        """
+        amount = 1  # default
+        if options.get('amount'):
+            try:
+                amount = int(options.get('amount'))
+            except ValueError:
+                amount = 1
+
+        phonebook_id = 1
+        if options.get('phonebook_id'):
+            try:
+                phonebook_id = options.get('phonebook_id')
+                phonebook_id = int(phonebook_id)
+            except ValueError:
+                phonebook_id = 1
+
+        prefix = ''
+        if options.get('prefix'):
+            prefix = options.get('prefix')
+
+        try:
+            obj_phonebook = Phonebook.objects.get(id=phonebook_id)
+        except:
+            print 'Can\'t find this Phonebook : %(id)s' % {'id': phonebook_id}
+            return False
+
+        length = 15
+        chars = "1234567890"
+        for k in range(1, int(amount) + 1):
+            if k % 1000 == 0:
+                print "%d contacts created..." % k
+            phone_no = ''.join([choice(chars) for i in range(length)])
+
+            # TODO: Use generate_series to speed up the contact creation
+            # INSERT INTO numbers (num) VALUES ( generate_series(1,1000));
+
+            try:
+                Contact.objects.create(
+                    contact=phone_no + prefix,
+                    phonebook=obj_phonebook)
+            except IntegrityError:
+                print "Error : Duplicate contact - %s" % phone_no
+
+        print "\nTotal contacts created : %(count)s" % {'count': amount}
